d4f66b444d4db6bfadbc02e751488b383b040f6e
Elie <niyubwayoiraelie5777@gmail.com>
2025-01-13 20:38:48 +0200

    Implement BentoGrid part 1

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,18 +1,24 @@
+
 import { gridItems } from "@/data"
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid"
 
 const Grid = () => {
   return (
     <section id="about">
-        <BentoGrid>
-            {gridItems.map((item) => (
+        <BentoGrid className="w-full py-20">
+            {gridItems.map(({id, title, description,className, img, imgClassName, titleClassName, spareImg}) => (
                 <BentoGridItem 
-                 id={item.id}
-                 key={item.id}
-                 title={item.title}
-                 description={item.description}
+                 id={id}
+                 key={id}
+                 title={title}
+                 description={description}
+                 className={className}
+                 img={img}
+                 imgClassName={imgClassName}
+                 titleClassName={titleClassName}
+                 spareImg={spareImg}
                 />
-            ))} 
+            ))}
         </BentoGrid>
        
     </section>
