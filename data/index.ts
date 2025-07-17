6a3493e9a5fd953a9948e7e04b52d718658f6fc8
Elie <niyubwayoiraelie5777@gmail.com>
2025-07-17 10:35:37 +0200

    Removed Sentry configuration and instrumentation files, and fixed lottie-web issues

--- data/index.ts
+++ data/index.ts
@@ -180,22 +180,22 @@ export const navItems = [
     },
     {
       id: 2,
-      title: "Mobile App Dev - JSM Tech",
+      title: "Mobile App Dev",
       desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp2.svg",
     },
     {
       id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      title: "Software Engineering Project",
+      desc: "Developed fullstack web app for a dev community, from initial concept to deployment on vercel.",
       className: "md:col-span-2", // change to md:col-span-2
       thumbnail: "/exp3.svg",
     },
     {
       id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      title: "Web Developer",
+      desc: "Developed and maintained user-facing features using modern frontend and backend technologies.",
       className: "md:col-span-2",
       thumbnail: "/exp4.svg",
     },
