1c23abf2c9660af996cb0594f35f7d31aa0fdec0
Elie <niyubwayoiraelie5777@gmail.com>
2025-07-24 08:32:33 +0200

    Refactor dependencies and add missing dependencies in UI components

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,25 +8,8 @@ import { useState } from "react";
 import Lottie from "react-lottie";
 import { IoCopyOutline } from "react-icons/io5";
 import dynamic from "next/dynamic";
-import InfiniteScroll from "../InfiniteScroll";
 const MagicButton = dynamic(() => import('./MagicButton'), { ssr: false });
 
-const items = [
-  { content: "Text Item 1" },
-  { content: <p>Paragraph Item 2</p> },
-  { content: "Text Item 3" },
-  { content: <p>Paragraph Item 4</p> },
-  { content: "Text Item 5" },
-  { content: <p>Paragraph Item 6</p> },
-  { content: "Text Item 7" },
-  { content: <p>Paragraph Item 8</p> },
-  { content: "Text Item 9" },
-  { content: <p>Paragraph Item 10</p> },
-  { content: "Text Item 11" },
-  { content: <p>Paragraph Item 12</p> },
-  { content: "Text Item 13" },
-  { content: <p>Paragraph Item 14</p> },
-];
 
 export const BentoGrid = ({
   className,
